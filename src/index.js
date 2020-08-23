e594f121391bac740755027dfad8355468c8946c
jengmicah <jengmicah@gmail.com>
2020-08-23 00:02:02 -0500

    Added template for endpoints (shorten, redirect)

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const yup = require('yup');
 const mongoose = require('mongoose');
 const { nanoid } = require('nanoid');
 
+require('dotenv').config();
+
 const app = express();
 app.enable('trust proxy');
 app.use(helmet());
@@ -24,6 +26,45 @@ mongoose
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
+// app.get('/', (req, res) => {
+//   res.json({ "response": "root" })
+// });
+
+const urlSchema = yup.object().shape({
+  slug: yup.string().trim().matches(/^[\w\-]+$/i),
+  url: yup.string().trim().url().required(),
+});
+
+app.post('/shortenUrl', async (req, res, next) => {
+  let { slug, url } = req.body;
+  try {
+    await urlSchema.validate({ slug, url });
+    // Validate URL
+    if (url.includes(process.env.URI)) {
+      throw new Error(`Try not to create a loop`);
+    }
+    // Validate slug
+    slug = slug || nanoid(5);
+    // TODO: check mongo for duplicates
+    const newRedirect = { slug, url };
+    // TODO: add slug and redirect to mongo
+    res.json(newRedirect);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get('/:slug', async (req, res, next) => {
+  const { slug } = req.params;
+  try {
+    // TODO: Get url mapped to the slug
+    // TODO: Redirect to that URL
+  } catch (error) {
+    // TODO: Or else, throw 404
+    console.log(error);
+  }
+});
+
 const port = 3000;
 
-app.listen(port, () => console.log('Server running...'));
+app.listen(port, () => console.log(`Server running on ${process.env.URI}`));
